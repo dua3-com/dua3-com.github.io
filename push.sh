ed61feb798161455bc5d695070906ec46dc77d2d
Axel Howind <axel@dua3.com>
2020-02-11 20:39:17 +0100

    push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
+#checkout github pages
+rm -r .gh-pages
+mkdir .gh-pages
+cd .gh-pages
+git init
+git remote add origin https://github.com/dua3-com/dua3-com.github.io.git
+git checkout -b gh-pages
+
+# clean contents
+rm -r ../.gh-pages/*
+
+# build the site
 jekyll build
-touch _site/.nojekyll
+
+# copy to github-pages
+cp -r ../_site/* ../.gh-pages/
+
+# create .nojekyll
+touch ../.gh-pages/.nojekyll
+
+# add files
+git add -A
+
+# commit
+git commit -m "update github-pages"
+
+# push
+git push origin gh-pages
